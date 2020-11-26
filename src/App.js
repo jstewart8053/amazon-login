fcc88ec0ab90699c853d8dd69f81829495cc39ed
Jennifer Stewart <jstewart8053@gmail.com>
2020-11-25 23:59:11 -0600

    set up files and text for form

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React from 'react';
-import { useEffect, useState } from 'react';
 import './App.css';
+import logo from './assets/logo.png';
+import Signup from './components/Signup';
 
 const App = () => {
 
-
-
-
-
   return (
     <div className='App'>
-      Amazon Login
+      <div className='outside-container'>
+        <img src={logo} alt='amazon logo' role="img" />
+        <Signup />
+      </div>
+      <footer>
+        <span> Conditions of Use </span>
+        <span> Privacy </span>
+        <span> Help </span>
+        <p> © 1996-2020, Amazon.com, Inc. or its affiliates </p>
+      </footer>
     </div>
   )
 }
---
+++ src/components/signup.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import '../App.css';
+import { useForm } from "react-hook-form";
+
+const Signup = () => {
+    const { register, handleSubmit, watch, errors } = useForm();
+    const onSubmit = data => {
+        console.log(data);
+    }
+    return (
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <div className='form-container'>
+                <h1> Create account </h1>
+                <label> Your name </label>
+                <label> Email </label>
+                <label> Password </label>
+                <label> Re-enter password </label>
+                <button input type='submit'> Create your Amazon account </button>
+                <p> By creating an account, you agree to Amazon's Conditions of Use and Privacy Notice. </p>
+                <hr />
+                <p> Already have an account? Sign-in </p>
+            </div>
+        </form>
+    )
+
+}
+export default Signup;
